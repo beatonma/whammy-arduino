a6390168226d54728893e47f34d7e681141311a9
Michael Beaton <beatonma@gmail.com>
2022-05-03 19:35:57 +0100

    Updated wiring config

--- config.h
+++ config.h
@@ -10,12 +10,12 @@ const double TEMPO_RANGE = (MAX_TEMPO - MIN_TEMPO);
 const uint8_t DEFAULT_PATCH = PATCH_WHAMMY_PLUS_ONE;
 const double DEFAULT_POSITION = 127.0;
 
-const uint8_t PIN_BUTTON_ON_OFF = 9;
-const uint8_t PIN_BUTTON_MODIFIER = 5;
-const uint8_t PIN_BUTTON_MODE = 7;
+const uint8_t PIN_BUTTON_ON_OFF = 4;
+const uint8_t PIN_BUTTON_MODIFIER = 8;
+const uint8_t PIN_BUTTON_MODE = 6;
 
-const uint8_t PIN_POT_TEMPO = 6;
-const uint8_t PIN_LED = 11;
+const uint8_t PIN_POT_TEMPO = 4;
+const uint8_t PIN_LED = 10;
 
 const uint8_t MAX_BRIGHTNESS = 90;
 
