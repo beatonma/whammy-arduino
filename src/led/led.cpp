12e205fd73cad5bc4ccea402f7fd9e551aee5f4b
Michael Beaton <beatonma@gmail.com>
2022-05-03 20:27:59 +0100

    Configured for RGB led (previously used single color led)..

--- src/led/led.cpp
+++ src/led/led.cpp
@@ -4,12 +4,57 @@
 
 namespace LED {
   namespace {
-    int currentBrightness = 0;
+    int currentBrightness = MAX_BRIGHTNESS;
+    double _red = 0.0;
+    double _green = 0.0;
+    double _blue = 255.0;
+
+    void setRgb(double r, double g, double b) {
+      _red = r;
+      _green = g;
+      _blue = b;
+    }
+
+    double normalize(uint8_t value) {
+      return (double) value / 255.0;
+    }
+
+    void write() {
+      const double brightness = normalize(currentBrightness);
+      analogWrite(PIN_LED_RED, _red * brightness);
+      analogWrite(PIN_LED_GREEN, _green * brightness);
+      analogWrite(PIN_LED_BLUE, _blue * brightness);
+      // analogWrite(PIN_LED_RED, 800);
+      // analogWrite(PIN_LED_GREEN, 800);
+      // analogWrite(PIN_LED_BLUE, 800);
+    }
+
+    void flash() {
+      on();
+      write();
+      delay(200);
+      off();
+    }
+  }
+
+  void setup() {
+    pinMode(PIN_LED_RED, OUTPUT);
+    pinMode(PIN_LED_GREEN, OUTPUT);
+    pinMode(PIN_LED_BLUE, OUTPUT);
+
+    red();
+    flash();
+
+    green();
+    flash();
+
+    blue();
+    flash();
   }
 
   void setBrightness(uint8_t brightness) {
     currentBrightness = max(0, min(brightness, MAX_BRIGHTNESS));
-    analogWrite(PIN_LED, currentBrightness);
+    write();
   }
 
   void set(bool _on) {
@@ -35,5 +80,33 @@ namespace LED {
 
   void fadeBy(int amount) {
     setBrightness(currentBrightness - amount);
+  }
+
+  void red() {
+    setRgb(255.0, 0.0, 0.0);
+
+  }
+  void green() {
+    setRgb(0.0, 255.0, 0.0);
+  }
+
+  void blue() {
+    setRgb(0.0, 0.0, 255.0);
+  }
+
+  void yellow() {
+    setRgb(255.0, 255.0, 0.0);
+  }
+
+  void cyan() {
+    setRgb(0.0, 255.0, 255.0);
+  }
+
+  void magenta() {
+    setRgb(255.0, 0.0, 255.0);
+  }
+
+  void white() {
+    setRgb(255.0, 255.0, 255.0);
   }
 }
--- src/led/led.h
+++ src/led/led.h
@@ -2,12 +2,21 @@
 #define H_LED
 
 namespace LED {
+  void setup();
   void setBrightness(uint8_t brightness);
   void set(bool on);
   void on();
   void off();
   void fade();
   void fadeBy(int amount);
+
+  void red();
+  void green();
+  void blue();
+  void yellow();
+  void cyan();
+  void magenta();
+  void white();
 }
 
 #endif
